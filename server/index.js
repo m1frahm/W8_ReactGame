54f4eff9b14d980422fb136b7c24a4984b1269c1
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-07 17:58:13 -0500

    trying out the try/catch from PP

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import fetch from 'node-fetch';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import fetch from "node-fetch";
 
-// For testing my API I saving one response in a .js file 
-import fakedata  from './fakedata.js';
+// For testing my API I saving one response in a .js file
+import fakedata from "./fakedata.js";
 
 const app = express();
 const PORT = 5000;
@@ -18,17 +18,30 @@ app.use(bodyParser.json());
 
 // //creates an endpoint for the route `/`
 app.get("/", (req, res) => {
-    res.json("Hello Techtonica Server for a Game");
-  });
+  res.json("Hello Techtonica Server for a Game");
+});
 
-// Make the GET request for the GAME Api for grabbing all the questions 
+// Make the GET request for the GAME Api for grabbing all the questions
 
+//comments out for now
+//   // //hardcode the game response for testing reasons to don't saturate my API call.
+// app.get('/api/game', (req, res) =>{
+//     res.json(fakedata);
+// })
 
-  // //hardcode the game response for testing reasons to don't saturate my API call. 
-app.get('/api/game', (req, res) =>{
-    res.json(fakedata);
-})
+//on back-end we are
+app.get("/api/game", async (req, res) => {
+  try {
+    const URL =
+      "https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=boolean";
+    const apiRequest = await fetch(URL); //gets data
+    const questions = await apiRequest.json(); // takes data from json format and converts into object
+    res.send({ questions }); //renders the questions
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-
-app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Sup! Server running on Port http://localhost:${PORT}`)
+);
